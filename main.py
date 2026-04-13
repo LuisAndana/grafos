7e332051d6f0a56f1c1dd69433b62ee80d971e10
LuisAndana <133995486+LuisAndana@users.noreply.github.com>
2026-04-13 15:32:30 -0700

    Revert "subir archivos"
    This reverts commit 1eacf17b5f128678ff7fe5c7f51a7344233e8c33.

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ from app.models.validacion import Validacion
 from app.models.tipo_usuario_proyecto import TipoUsuarioProyecto
 from app.models.caso_uso import CasoUso
 from app.models.restriccion import Restriccion
-from app.models.artefacto import Artefacto
 
 # ════════════════════════════════════════════════════════════════════════════
 # ROUTERS
@@ -43,7 +42,6 @@ from app.routes.srs_router import router as srs_router
 from app.routes.tipo_usuario_router import router as tipo_usuario_router
 from app.routes.caso_uso_router import router as caso_uso_router
 from app.routes.restriccion_router import router as restriccion_router
-from app.routes.artefacto_router import router as artefacto_router
 
 settings = get_settings()
 
@@ -73,7 +71,6 @@ app.include_router(srs_router)
 app.include_router(tipo_usuario_router)
 app.include_router(caso_uso_router)
 app.include_router(restriccion_router)
-app.include_router(artefacto_router)
 
 
 def custom_openapi():
